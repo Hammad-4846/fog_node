0e68a12a9ea9d3dac983bdb538245bbaa5d2f73b
Hammad-4846 <curious4846@gmail.com>
2023-05-18 22:21:51 +0530

    Updating

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const { loginUser, registerUser } = require("../controllers/userController");
+const { loginUser, registerUser, getAllUsers, deleteUser } = require("../controllers/userController");
 
 router.route("/auth/login").post(loginUser);
 router.route("/auth/register").post(registerUser);
+router.route("/admin/users").get(getAllUsers);
+router
+  .route("/admin/user/:id")
+  .delete(deleteUser);
 
 module.exports = router;
